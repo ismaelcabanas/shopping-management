2d96269e024a07297fe5db21b5e3085190a5d019
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2025-10-17 18:04:48 +0200

    fix: e2e in CI

--- shopping-management-webapp/vitest.config.ts
+++ shopping-management-webapp/vitest.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'
+    ],
   },
   resolve: {
     alias: {
@@ -16,4 +23,3 @@ export default defineConfig({
     },
   },
 })
-
